126e397ac924a650fffc999fc3914a8f3e38b376
jeninacunanan <jenina.cunanan@gmail.com>
2015-07-17 17:24:23 +0800

    Updated Candidate and Position Controllers
    Filled up CRUD for both

--- votingapp/app/controllers/candidate_controller.rb
+++ votingapp/app/controllers/candidate_controller.rb
@@ -1,4 +1,6 @@
 class CandidateController < ApplicationController
+  before_action :set_candidate, only: [:show, :edit, :update, :destroy]
+
   def index
     @candidates = Candidate.all
   end
@@ -7,17 +9,41 @@ class CandidateController < ApplicationController
   end
 
   def new
+    @candidate = Candidate.new
   end
 
   def create
+    @candidate = Candidate.new(candidate_params)
+    if @candidate.save
+      redirect_to @candidate
+    else
+      render 'new'
+    end
   end
 
   def edit
+     @candidate = Candidate.find(params[:id])
   end
 
   def update
+    if @candidate.update(candidate_params)
+      redirect_to @candidate
+    else
+      render 'edit'
+    end
   end
 
   def destroy
+    @candidate.destroy
+    redirect_to candidates_path
   end
+
+  private
+    def candidate_params
+      params.require(:candidate).permit!
+    end
+
+    def set_candidate
+      @candidate = Candidate.find(params[:id])
+    end
 end
--- votingapp/app/controllers/position_controller.rb
+++ votingapp/app/controllers/position_controller.rb
@@ -1,22 +1,49 @@
 class PositionController < ApplicationController
+  before_action :set_position, only: [:show, :edit, :update, :destroy]
+
   def index
+    @positions = Position.all
   end
 
   def show
   end
 
   def new
+    @position = Position.new
   end
 
   def create
+    @position = Position.new(position_params)
+    if @position.save
+      redirect_to @position
+    else
+      render 'new'
+    end
   end
 
   def edit
+     @position = Position.find(params[:id])
   end
 
   def update
+    if @position.update(position_params)
+      redirect_to @position
+    else
+      render 'edit'
+    end
   end
 
   def destroy
+    @position.destroy
+    redirect_to positions_path
   end
+
+  private
+    def position_params
+      params.require(:position).permit!
+    end
+
+    def set_position
+      @position = Position.find(params[:id])
+    end
 end
