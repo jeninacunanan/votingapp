17ace1e2a99ce8893f37b40567f94bfad6587ba6
Mico <enrico.bongato@gmail.com>
2015-07-17 23:06:43 +0800

    Calendar + New Gems Fixed
    Added Calendar Gem + Fixed Reg. Form

--- votingapp/app/models/user.rb
+++ votingapp/app/models/user.rb
@@ -11,6 +11,6 @@ class User < ActiveRecord::Base
 
 	validates :first_name, presence: :true
 	validates :last_name, presence: :true
-	# validates :birthday, presence: :true
+	validates :birthday, presence: :true
 	validates :gender, presence: :true
 end
